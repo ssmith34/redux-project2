0f3396abca6282dda57ca3517130ea30ac98ac01
349078 <ShaneSmith@fnbo.com>
2023-09-26 11:14:58 -0400

    memory issues on work laptop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cart from './components/cart/Cart';
+import Layout from './components/layout/Layout';
+import Products from './components/shop/Products';
+
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
